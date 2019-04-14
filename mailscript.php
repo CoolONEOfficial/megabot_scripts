cc66e1abfa89a1f12a2179d7143b4093952028cc
coolone <coolone.official@gmail.com>
2019-04-14 15:02:51 +0300

    Fixed py call

--- mailscript.php
+++ mailscript.php
@@ -51,4 +51,4 @@ foreach ($messages as $message) {
 
 $connection->expunge();
 
-exec('~/anaconda/bin/python3 ~/domains/cometbot.ru/pyscript/bot.py');
+exec('~/anaconda/bin/python ~/domains/cometbot.ru/pyscript/bot.py');
